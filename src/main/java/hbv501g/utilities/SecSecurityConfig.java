6ddbd3408489bf0af5e4dd4df2a17ddf2debd5da
YellowWall <89491565+YellowWall@users.noreply.github.com>
2023-09-26 13:46:19 +0000

    start af login forritun

---
+++ src/main/java/hbv501g/utilities/SecSecurityConfig.java
@@ -0,0 +1,59 @@
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        // InMemoryUserDetailsManager (see below)
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                UserDetails user1 = User.withUsername("user1")
+            .password(passwordEncoder().encode("user1Pass"))
+            .roles("USER")
+            .build();
+        UserDetails user2 = User.withUsername("user2")
+            .password(passwordEncoder().encode("user2Pass"))
+            .roles("USER")
+            .build();
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder().encode("adminPass"))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+       @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+            .disable()
+            .authorizeRequests()
+            .antMatchers("/admin/**")
+            .hasRole("ADMIN")
+            .antMatchers("/anonymous*")
+            .anonymous()
+            .antMatchers("/login*")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login.html")
+            .loginProcessingUrl("/perform_login")
+            .defaultSuccessUrl("/homepage.html", true)
+            .failureUrl("/login.html?error=true")
+            .failureHandler(authenticationFailureHandler())
+            .and()
+            .logout()
+            .logoutUrl("/perform_logout")
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler(logoutSuccessHandler());
+        return http.build();
+    }
+}
