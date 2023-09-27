5a3d4be3dc1379c373e91ee0bb6f873512fc63c0
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2023-09-27 19:17:32 +0000

    Breyta í UserController.

--- src/main/java/hbv501g/objects/Hole.java
+++ src/main/java/hbv501g/objects/Hole.java
@@ -3,6 +3,6 @@ package hbv501g.objects;
 public class Hole{
     long gameId;
     long playerId;
-    int yeets; // throws má ekki vera því það er yava function til að kasta villu
+    int yeets; // throws má ekki vera því það er java function til að kasta villu
     int throwLength;
 }
