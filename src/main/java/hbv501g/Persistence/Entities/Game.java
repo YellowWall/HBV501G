880c67f4d25229998e0aa09afea25c9937c04887
YellowWall <89491565+YellowWall@users.noreply.github.com>
2023-10-18 12:00:59 +0000

    meiri vinna í gameController
    þurfum að finna einhverja leið til að virka game_id_seq

--- src/main/java/hbv501g/Persistence/Entities/Game.java
+++ src/main/java/hbv501g/Persistence/Entities/Game.java
@@ -26,10 +26,6 @@ public class Game{
         this.date = date;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public void setScore(String score) {
         this.score = score;
     }
