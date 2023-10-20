6d512b526e2fbf9d73ecae66c3a160bf47a3472f
YellowWall <89491565+YellowWall@users.noreply.github.com>
2023-10-20 14:13:14 +0000

    field fært yfir í persistence entities, tafla stofnuð í gagnagrunni

---
+++ src/main/java/hbv501g/Persistence/Repositories/FieldRepository.java
@@ -0,0 +1,9 @@
+package hbv501g.Persistence.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hbv501g.Persistence.Entities.Field;
+
+public interface FieldRepository extends JpaRepository<Field,Long> {
+    
+}
