125fb6ff826135371dff6909ea94af3434268709
dha5 <dha5@hi.is>
2023-10-21 19:17:24 +0100

    bætti við nokkrum föllum í FieldService

--- src/main/java/hbv501g/Persistence/Repositories/FieldRepository.java
+++ src/main/java/hbv501g/Persistence/Repositories/FieldRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hbv501g.Persistence.Entities.Field;
 
 public interface FieldRepository extends JpaRepository<Field,Long> {
-    
+
+    Field findByFieldname();
 }
--- src/main/java/hbv501g/Services/FieldService.java
+++ src/main/java/hbv501g/Services/FieldService.java
@@ -1,8 +1,28 @@
 package hbv501g.Services;
 
+import hbv501g.Persistence.Repositories.FieldRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FieldService {
-    
+    @Autowired
+    private FieldRepository fieldRepository;
+
+    public List<Field> getAllFields(){
+        List<Field> fields = fieldRepository.findAll();
+        return fields;
+    }
+
+    public Field getField(String fieldname){
+        Field field = fieldRepository.findByFieldname();
+        return field;
+    }
+
+    public Field saveField(Field field){
+        return fieldRepository.save(field);
+    }
+
+
+
 }
