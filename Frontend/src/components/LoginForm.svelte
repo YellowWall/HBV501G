18f77b9f49983870248d6d51376a1d0bda0cb158
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2023-11-08 15:20:05 +0000

    Login form útlit

--- Frontend/src/components/LoginForm.svelte
+++ Frontend/src/components/LoginForm.svelte
@@ -1,17 +1,23 @@
 <script>
     
 </script>
-
-<div>
+<h1>Welcome to Folf Tracker</h1>
+<h2>Login</h2>
+<div class="form">
     <form action="">
         <input 
         type="text"
         name="username"
+        placeholder="Name"
         >
         <input 
         type="text"
         name="pasword"
+        placeholder="Password"
         >
+        <input 
+        type="submit"
+        value="Login">
     </form>
 </div>
 
--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -2,8 +2,7 @@
     import LoginForm from '../components/LoginForm.svelte';
 </script>
 
-<h1>Welcome to Folf Tracker</h1>
-<h2>Login</h2>
+
 
 <section>
     <LoginForm />
