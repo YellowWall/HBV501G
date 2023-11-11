44dcc5b63c80b4ec62e69859b5dff3c9ea933d22
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2023-11-11 15:54:54 +0000

    láta async virka í user homepage...allavega smá

--- Frontend/src/routes/homepage/+page.svelte
+++ Frontend/src/routes/homepage/+page.svelte
@@ -21,18 +21,22 @@
             //console.log('her er res');
             //console.log(res);
             const data = await res.json();
-            //console.log('her er data');
-            //console.log(data);
+            console.log('her er data');
+            console.log(data);
             if (res.ok) {
                 return data;
             }
         }
 
-        let user = load();
+        let user =load();
         console.log("her er user")
         console.log(user.data);
 </script>
 
 
 <h1>This is the user homepage</h1>
-<p></p>
+{#await user}
+    <p>Fetching your info</p>
+{:then user} 
+    <p>{user.data.username}</p>
+{/await}
