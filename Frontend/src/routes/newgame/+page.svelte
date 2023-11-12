f9c517a5b74c37b53969d53254a974a1bbde5dc1
dha5 <dha5@hi.is>
2023-11-12 01:38:33 +0000

    skapaði new game síðu og setti upp flowbite

---
+++ Frontend/src/routes/newgame/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+    let backendRoute = 'http://localhost:8080';
+    let selected ='select field';
+    let options = [
+        'select field',
+        'field 1',
+        'field 2'
+    ]
+    $: console.log('changed selected:', selected)
+    $: console.log('updated options:', options)
+</script>
+
+<h1>new game</h1>
+
+<select bind:value={selected}>
+    {#each options as value}<option {value}>{value  }</option>{/each}
+</select>
+
+<table shadow>
+    <TableHead>
+        <TableHeadCell>hole number</TableHeadCell>
+        <TableHeadCell>number of throws</TableHeadCell>
+    </TableHead>
+    <TableBody class="divide-y">
+        <TableBodyRow>
+            <TableBodyCell>nr 1</TableBodyCell>
+            <TableBodyCell>nr 2</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell><input type="text"></TableBodyCell>
+            <TableBodyCell><input type="text"></TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+</table>
