f93932589616367d274e858705ebbcc516610fc0
dha5 <dha5@hi.is>
2023-11-13 14:53:27 +0000

    new game síða uppfærð

--- Frontend/src/routes/newgame/+page.svelte
+++ Frontend/src/routes/newgame/+page.svelte
@@ -2,10 +2,16 @@
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
     let backendRoute = 'http://localhost:8080';
     let selected ='select field';
+    //þarf að skipta options út fyrir velli úr bakenda
     let options = [
         'select field',
         'field 1',
         'field 2'
+    ]
+    let hole = [
+        '1',
+        '2',
+        '3',
     ]
     $: console.log('changed selected:', selected)
     $: console.log('updated options:', options)
@@ -23,13 +29,11 @@
         <TableHeadCell>number of throws</TableHeadCell>
     </TableHead>
     <TableBody class="divide-y">
-        <TableBodyRow>
-            <TableBodyCell>nr 1</TableBodyCell>
-            <TableBodyCell>nr 2</TableBodyCell>
-        </TableBodyRow>
-        <TableBodyRow>
-            <TableBodyCell><input type="text"></TableBodyCell>
-            <TableBodyCell><input type="text"></TableBodyCell>
-        </TableBodyRow>
+        {#each hole as holes, index}
+            <TableBodyRow>
+                {holes}
+                <TableBodyCell><input type="number"></TableBodyCell>
+            </TableBodyRow>
+        {/each}
     </TableBody>
 </table>
