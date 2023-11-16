991c99fe5e835948ede238a2d90ea2b1e806b7a9
dha5 <dha5@hi.is>
2023-11-16 13:10:13 +0000

    new game uppfært til að vinna með bakenda

--- Frontend/src/routes/newgame/+page.svelte
+++ Frontend/src/routes/newgame/+page.svelte
@@ -1,39 +1,9 @@
 <script>
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
-    let backendRoute = 'http://localhost:8080';
-    let selected ='select field';
-    //þarf að skipta options út fyrir velli úr bakenda
-    let options = [
-        'select field',
-        'field 1',
-        'field 2'
-    ]
-    let hole = [
-        '1',
-        '2',
-        '3',
-    ]
-    $: console.log('changed selected:', selected)
-    $: console.log('updated options:', options)
+	import NewGame from '../../components/NewGame.svelte';
 </script>
 
-<h1>new game</h1>
 
-<select bind:value={selected}>
-    {#each options as value}<option {value}>{value  }</option>{/each}
-</select>
 
-<table shadow>
-    <TableHead>
-        <TableHeadCell>hole number</TableHeadCell>
-        <TableHeadCell>number of throws</TableHeadCell>
-    </TableHead>
-    <TableBody class="divide-y">
-        {#each hole as holes, index}
-            <TableBodyRow>
-                {holes}
-                <TableBodyCell><input type="number"></TableBodyCell>
-            </TableBodyRow>
-        {/each}
-    </TableBody>
-</table>
+<section>
+    <NewGame />
+</section>
