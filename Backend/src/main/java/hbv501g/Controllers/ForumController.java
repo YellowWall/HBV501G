90d0053397e7e20d58c15e2b4aad831a1c96bd88
YellowWall <89491565+YellowWall@users.noreply.github.com>
2023-11-18 21:15:22 +0000

    Update ForumController.java

--- Backend/src/main/java/hbv501g/Controllers/ForumController.java
+++ Backend/src/main/java/hbv501g/Controllers/ForumController.java
@@ -3,15 +3,7 @@ package hbv501g.Controllers;
 import java.util.List;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 import hbv501g.Classes.JsonResponse;
 import hbv501g.Services.ForumService;
@@ -21,7 +13,7 @@ import hbv501g.Persistence.Entities.User;
 import hbv501g.objects.ReturnPost;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin
 @RequestMapping("/forum")
 public class ForumController {
     @Autowired
